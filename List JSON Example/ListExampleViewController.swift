6d57685545bcb538e27e674ba6aaaaee4c35877f
Jonas Bylund <jonas.b.bylund@gmail.com>
2020-06-07 17:09:25 +0200

    Create and setup project

---
+++ List JSON Example/ListExampleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  List JSON Example
+//
+//  Created by Jonas Bylund on 2020-06-07.
+//  Copyright © 2020 Jonas Bylund. All rights reserved.
+//
+
+import UIKit
+
+class ListExampleViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
